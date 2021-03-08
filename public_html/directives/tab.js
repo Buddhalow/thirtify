03dc64d5c793c410a1eac584af96a01b7d912d94
Alexander Forselius <alexander.forselius@buddhalow.com>
2021-03-08 13:38:18 +0100

    Working on stuff

--- public_html/directives/tab.js
+++ public_html/directives/tab.js
@@ -7,28 +7,7 @@
 				section: '=section',
 				label: '@'
 			},
-			templateUrl: '/partials/tab.html',
-			compile: function (element, attributes) {
-
-				//linkFunction is linked with each element with scope to get the element specific data.
-				var linkFunction = function($scope, element, attributes) {
-					var hash = window.location.hash.substr(1)
-					if (!hash || hash.length < 1) {
-						hash = 'overview'
-					}
-					element.html(attributes.label)
-
-					if (attributes.section == hash) {
-						element.addClass('active')
-					}
-				 	element.mousedown(function () {
-						console.log(attributes.section)
-			      		window.location.href = window.location.href.split('#')[0] + '#' + attributes.section
-			      	})
-				}
-				return linkFunction;
-
-			}
+			templateUrl: '/partials/tab.html'
 		};
 	});
 })();
